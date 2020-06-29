2e7c42ce6ce3a1cef1ea05aebdc841fe4fbfe05d
TimeEror123 <67280289+TimeEror123@users.noreply.github.com>
2020-06-30 02:52:59 +0700

    Create tool.sh

---
+++ tool.sh
@@ -0,0 +1,109 @@
+clear
+cowsay -f ghostbusters "W A R N I N G" | lolcat
+echo
+echo "----------------------[BY : TIME EROR]-----------------"|lolcat
+echo "----------------[JANGAN DI SALAH GUNAKAN TODD]---------"|lolcat
+echo "----------------------[ASSASIN CYBERR]-----------------"|lolcat
+echo
+echo "PILIH MENU TOOLS"|lolcat
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" DARK NEW        "$i"|"$i"    |"$cy" B"$i" |"|lolcat
+echo $i"|"$me" 2"$i" |"$cy" PHISING     "$i"|"$i"    |"$cy" E"$i" |"|lolcat
+echo $i"|"$me" 3"$i" |"$cy" MBF      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"|"$me" 4"$i" |"$cy" Hack FREE FIRE      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"|"$me" 5"$i" |"$cy" HACK PULSA      "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" HACK WHATSAPP      "$i"|"$i"    |"$cy" N"$i" |"|lolcat
+echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"|lolcat
+echo 
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+git clone https://github.com/Fajar-24/CyberNEW
+cd CyberNEW
+python2 Cyber.py
+fi
+
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+git clone https://github.com/Fajar-24/phising
+cd phising
+sh v2.sh
+fi
+
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+git clone https://github.com/Fajar-24/CendolDawet
+cd CendolDawet
+python2 Hack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+echo " TUNGGU SEBENTAR SCRIPT AKAN DI INSTALL "|lolcat
+echo " SCRIPT AKAN DI INSTALL TUNGGU 5 DETIK "|lolcat
+figlet "5"|lolcat
+figlet "4"|lolcat
+figlet "3"|lolcat
+figlet "2"|lolcat
+figlet "1"|lolcat
+clear
+git clone https://github.com/Fajar-24/FreeFire
+cd FreeFire
+sh Fire.sh
+fi
+
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+echo " TUNGGU SEBENTAR SCRIPT AKAN DI INSTALL "|lolcat
+echo " SCRIPT AKAN DI INSTALL TUNGGU 5 DETIK "|lolcat
+figlet "5"|lolcat
+figlet "4"|lolcat
+figlet "3"|lolcat
+figlet "2"|lolcat
+figlet "1"|lolcat
+git clone https://github.com/Fajar-24/HACKPULSA
+cd HACKPULSA
+sh HACKPULSA.sh
+fi
+
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "B A C O T"|lolcat
+sleep 1
+echo " TUNGGU SEBENTAR SCRIPT AKAN DI INSTALL "|lolcat
+echo " SCRIPT AKAN DI INSTALL TUNGGU 5 DETIK "|lolcat
+figlet "5"|lolcat
+clear
+figlet "4"|lolcat
+clear
+figlet "3"|lolcat
+clear
+figlet "2"|lolcat
+clear
+figlet "1"|lolcat
+git clone https://github.com/Fajar-24/HACKWHATSAPP
+cd HACKWHATSAPP
+sh WhatSapp.sh
+fi
